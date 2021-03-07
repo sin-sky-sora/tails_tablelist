87f7b7b2a3a6e49c757b17757dc6d04441ec57c7
name <email@example.com>
2021-03-07 16:47:12 +0000

    urls fix

--- tails/urls.py
+++ tails/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path,include
-from django.views.generic.simple import redirect_to
 from .views import index,home,trade,trande_post
 urlpatterns = [
     path('',index,name="top"),
