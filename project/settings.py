65609a6a777295c5c33147bb4b62d287fc34af97
name <email@example.com>
2021-03-07 07:54:54 +0000

    gunicorn added

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -122,4 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "product/"
 STATICFILES_DIR = [os.path.join(BASE_DIR,"static")]
