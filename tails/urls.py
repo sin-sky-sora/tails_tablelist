a6876d642582c274340951fd2ae627089e7253a3
name <email@example.com>
2021-03-07 16:45:43 +0000

    url pattern added

--- tails/urls.py
+++ tails/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,include
-from .views import index
+from django.views.generic.simple import redirect_to
+from .views import index,home,trade,trande_post
 urlpatterns = [
-    path('',index,name="index"),
+    path('',index,name="top"),
+    path('home/',home,name="home"),
+    path('trade/',trade,name="trade"),
+    path('trade/post/',trande_post,name="trade_post"),
 ]
--- tails/views.py
+++ tails/views.py
@@ -2,4 +2,13 @@ from django.http.response import HttpResponse,JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
 
 def index(request):
-    return render(request,"index.html")
+    return redirect("home")
+    
+def home(request):
+    return render(request,"home.html")
+
+def trade(request):
+    return render(request,"trade.html")
+
+def trande_post(request):
+    return render(request,"trade.post.html")
